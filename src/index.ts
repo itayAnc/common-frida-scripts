9075abc39e8552c6dd6ff5d8e0db487dfd434a64
Itay Hadad <itay@sayanchor.com>
2024-06-28 18:05:27 +0300

    exporting utils

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,33 @@ import { print_exception_stack_trace, print_thread_stack_trace, print_throwable_
 import { clone_clonable, clone_parcelable, clone_serializable, parse_parcelable, serialize_parcelable } from "./deep_clone"
 import { file_read_bytes, file_read_serializable, file_read_string, file_write_serializable, file_write_string } from "./file_io"
 import { attempt_disable_ads, attempt_disable_facebook_ads, attempt_disable_google_mobile_ads, attempt_disable_unity_ads, attempt_set_vungle_disabled } from "./disable_ads"
+
+export {
+    attempt_disable_ads,
+    attempt_disable_facebook_ads,
+    attempt_disable_google_mobile_ads,
+    attempt_disable_unity_ads,
+    attempt_set_vungle_disabled,
+    clone_clonable,
+    clone_parcelable,
+    clone_serializable,
+    class_exists,
+    file_read_bytes,
+    file_read_serializable,
+    file_read_string,
+    file_write_serializable,
+    file_write_string,
+    get_class_instances,
+    get_class_object,
+    get_object_address,
+    get_process_modules,
+    parse_parcelable,
+    print_exception_stack_trace,
+    print_object_properties,
+    print_thread_stack_trace,
+    print_throwable_stack_trace,
+    serialize_parcelable,
+    show_toast,
+    toast_loop,
+    write_int
+}
